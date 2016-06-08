2af72aa0f12acecfb7dc79cea7f2e1f8e0b86294
Tsumaranai <zbytyousan@gmail.com>
2016-06-08 16:35:10 +0800

    works! version 0.8 waiting for modify

--- pkt.py
+++ pkt.py
@@ -4,8 +4,8 @@ import ctypes
 import socket
 class icmp():
     def checksum(self, data_B):
-        if len(data_B)/2:
-            data_B + '\x00'
+        if len(data_B)&0x1:
+            data_B += '\x00'
 
         data_H = struct.unpack("!%sH" % (len(data_B)/2), data_B)
         _sum = 0
@@ -29,26 +29,26 @@ class icmp():
     def parse(self, data):
         pktfmt = "!BBHHH%ss" % len(data[8:])
 
-        self._type, self.code, self.cksum, self.iden, self.NO, self.info = \
+        self._type, self.code, self.cksum, self.iden, self.seqNO, self.info = \
                 struct.unpack(pktfmt, data)
 
         #print self._type, self.code, self.cksum, self.iden, self.NO, self.info
 
-        return data[28:]
+        return data[8:]
 
 class ip():
 
     def parse(self, data):
 	
-	head_len = struct.unpack("B",data[0])
-
-        head_len >>= 4
+	head_lenB = struct.unpack("B",data[0])
+        head_len = head_lenB[0]
+        head_len &=0xf
         head_len *= 4
-
+        #print head_len
         self.hlen = head_len
         self.datalen = len(data) - head_len
 
-	src, dst = data[12: 16], data[16: 20]
-        self.src = socket.inet_ntoa(self.src)
-        self.dst = socket.inet_ntoa(self.dst)
+	self.nsrc, self.ndst = data[12: 16], data[16: 20]
+        self.asrc = socket.inet_ntoa(self.nsrc)
+        self.adst = socket.inet_ntoa(self.ndst)
         return data[head_len: ]
--- tun.py
+++ tun.py
@@ -8,26 +8,28 @@ import select
 
 TUNSETIFF = 0x400454ca
 IFF_TUN   = 0x0001
+IFF_NO_PI=0x0
 IP = "10.10.10.10"
-DEV = "eth0"
-MTU = 1000
+DEV = "ens33"
+MTU = 1200
 MAGIC = "zby"
 REQ = 8
 RPY = 0
-SER_IP = "0.0.0.0"
+SER_IP = "192.168.111.139"
 TIME_OUT = 120
 
 class tun():
 
     def create(self):
         self.tfd = os.open("/dev/net/tun", os.O_RDWR)
-        ifname = fcntl.ioctl(self.tfd, TUNSETIFF, struct.pack("16sH", "tun%d", IFF_TUN))
+        ifname = fcntl.ioctl(self.tfd, TUNSETIFF, struct.pack("16sH", "tun%d", IFF_TUN|IFF_NO_PI))
         self.name = ifname[:16].strip("\x00")
 
     def config(self):
         self.ip = IP
         self.tunip = "10.0.0.1"
-        os.system("ifconfig %s  %s pointopoint %s up" % (self.name, self.ip, self.tunip))
+        #os.system("ifconfig %s  %s pointopoint %s up" % (self.name, self.ip, self.tunip))
+        os.system("ifconfig %s  %s up" % (self.name, self.ip))
         os.system("ip link set %s mtu %s" % (self.name, MTU))
     def ping_reply(self, iden, seqNO, data):        
         buf = pkt.create(0, 0, 0, iden, seqNO, data)
@@ -57,17 +59,17 @@ class tun_c(tun):
     def start(self):
         self.icmpfd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
 
-        rst = select.select([self.icmpfd, self.tfd], [], [], TIME_OUT)[0]
         seqNO = 1
         iden = 0x1234
 
         while True:
-
+            
+            rst = select.select([self.icmpfd, self.tfd], [], [], TIME_OUT)[0]
             for fd in rst:
 
                 if fd == self.icmpfd:
                     #reply data from the server
-                    buf = os.read(fd, 1500)
+                    buf = self.icmpfd.recv(1500)
 
                     ipkt = pkt.ip()
                     data = ipkt.parse(buf)
@@ -83,9 +85,10 @@ class tun_c(tun):
 
                 elif fd == self.tfd:
                     #this is from our system kernel
-                    data = os.read(fd, MTU)
+                    data = os.read(self.tfd, MTU)
+                    icmpkt = pkt.icmp()
                     buf = icmpkt.create(RPY, 0, 0, iden, seqNO, MAGIC + data)
-                    self.icmpfd(buf, (SER_IP, 0))
+                    self.icmpfd.sendto(buf, (SER_IP, 0))
                     seqNO += 1
 
                 else:
@@ -98,62 +101,74 @@ class tun_c(tun):
         #os.system("iptables -t nat -D PREROUTING -i %s -p icmp  -j DNAT --to-destination  %s"% (DEV, self.tunip))
         #os.close(self.fd)
         #exit()
-
-
 class tun_s(tun):
 
     def set_rule(self):
+        os.system("route add -net 10.10.10.0/24 dev %s" % self.name)
+        #os.system("ip link set %s mtu %s"%(DEV, MTU))
         os.system("iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o %s -j MASQUERADE"% DEV)
+        #os.system("route add default gw 192.168.122.1 dev virbr0")
         #os.system("iptables -t nat -A PREROUTING -i %s -p icmp -j DNAT --to-destination %s", (DEV, self.tunip))
-
-    def end(self):
+        pass
+        
+    def end(self): 
+        os.system("route del -net 10.10.10.0/24 dev %s" % self.name)
+        #os.system("ip link set %s mtu %s"%(DEV, 1500))
         os.system("iptables -t nat -D POSTROUTING -s 10.10.10.0/24 -o %s -j MASQUERADE"% DEV)
+        #os.system("route del default")
         #os.system("iptables -t nat -D PREROUTING -i %s -p icmp -j DNAT --to-destination %s", (DEV, self.tunip))
         #os.close(self.fd)
-
+        pass
+        
     def start(self):
 
         self.icmpfd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
-        self.client = {}
+        client = {}
         #this place should set a while to infinite loop
-        rst = select.select([self.icmpfd, self.tfd], [], [], TIME_OUT)[0]
-
         while True:
+            rst = select.select([self.icmpfd, self.tfd], [], [], TIME_OUT)[0]
+
+        #while True:
 
             for fd in rst:
 
                 if fd == self.tfd:
                     #this message from a webip
-                    data = os.read(fd, MTU)
+                    data = os.read(self.tfd, MTU)
                     appkt = pkt.ip()
-                    appkt.parse(data)
+                    appkt.parse(data[4:])
+                    print client 
+                    print (appkt.adst, appkt.asrc)
                     for t_id in client:
-                        
-                        if client[t_id]["appsip"] == appkt.dst:
+                        if client[t_id]["appsip"] == appkt.ndst:
                             #find which clinet requested this message
+                            
                             icmpkt = pkt.icmp()
-                            buf = icmpkt.create(RPY, 0, 0, client[t_ip]["iden"], client[t_ip]["seq"], MAGIC+data)
-                            self.icmpfd.sendto(buf, (client[t_ip]["ip"], 0))
+                            buf = icmpkt.create(RPY, 0, 0, client[t_id]["iden"], client[t_id]["seq"], MAGIC+data)
+                            self.icmpfd.sendto(buf, (client[t_id]["ip"], 0))
                             break
                             
                 elif fd == self.icmpfd:
                     #this message from a client
                     
-                    data = os.read(fd, MTU)
+                    data = self.icmpfd.recv(MTU)
                     
                     ipkt = pkt.ip()
                     buf = ipkt.parse(data)
                     
                     icmpkt = pkt.icmp()
                     buf = icmpkt.parse(buf)
-                    
+                    #print icmpkt._type, buf[:3]
                     if icmpkt._type == RPY and buf[:3] == MAGIC:
                         #this is a tunnel request, 3 is the length of MAGIC
                         appkt = pkt.ip()
-                        appkt.parse(buf[3:])
+                        appkt.parse(buf[7:])
                         
-                        ID = struct.pack("4sH",(ipkt.src, icmpkt.iden))
-                        client[ID] = {"ip": ipkt.asrc, "iden": icmpkt.iden, "seq": icmpkt.seqNO, "appdip": appkt.dst, "appsip":appkt.src}
+                        ID = struct.pack("4sH",ipkt.nsrc, icmpkt.iden)
+                        print appkt.asrc, appkt.adst
+                        #print buf[3:]
+                        print ipkt.asrc, ipkt.adst
+                        client[ID] = {"ip": ipkt.asrc, "iden": icmpkt.iden, "seq": icmpkt.seqNO, "appdip": appkt.ndst, "appsip":appkt.nsrc}
                         os.write(self.tfd, buf[3:])
                     else:
                         pass
@@ -163,10 +178,14 @@ class tun_s(tun):
                     client = {}
 
 #icmpfd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
-while True:
-    try:
-        #buf, add = icmpfd.recvfrom(MTU)
-        #print buf
-    except KeyboardInterrupt:
-        t.end()
-        t.close()
+t = tun_s()
+t.create()
+t.config()
+t.set_rule()
+
+try:
+    t.start()
+    
+except KeyboardInterrupt:
+    t.end()
+    t.close()
